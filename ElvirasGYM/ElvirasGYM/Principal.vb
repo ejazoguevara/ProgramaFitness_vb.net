c26e679d573dbc6b7060bcb154a4d0653da58dfd
Efrain Jazo <jage_82@hotmail.com>
2014-11-18 23:04:09 -0600

    Programa final en VB.net

---
+++ ElvirasGYM/ElvirasGYM/Principal.vb
@@ -0,0 +1,59 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.Threading
+
+Public Class Principal
+    Private Const server As String = "localhost"
+    Private Const BD As String = "elvirasgym"
+    Private Const user As String = "root"
+    Private Const pass As String = ""
+    Dim datos As MySqlDataReader
+
+
+    Dim conectar As New BDMysql(server, BD, user, pass)
+    Dim Sql As String
+
+    Private Sub Principal_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Show()
+        lblFecha.Text = Date.Now.ToShortDateString
+        lblFecha1.BackColor = Color.Transparent
+        lblEmpleado1.BackColor = Color.Transparent
+        conectar.open()
+        Thread.Sleep(500)
+        Sql = "SELECT * FROM usuarios WHERE activo = 1"
+        datos = conectar.executeReader(Sql)
+        If datos.Read = True Then
+            lblEmpleado.Text = datos("nombre").ToString & " " & datos("apellidos").ToString
+        End If
+        datos.Close()
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SalirToolStripMenuItem.Click
+        Sql = "UPDATE usuarios SET activo = 0"
+        If conectar.executeSQL(Sql) Then
+            Login.Close()
+            Close()
+        End If
+    End Sub
+
+    Private Sub CerrarSesiónToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CerrarSesiónToolStripMenuItem.Click
+        Sql = "UPDATE usuarios SET Activo = 0"
+        If conectar.executeSQL(Sql) Then
+            Login.Show()
+            Me.Dispose()
+        End If
+    End Sub
+
+    Private Sub btnNuevocliente_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnNuevocliente.Click
+        Clientes.Show()
+        Clientes.tabcliente.SelectTab(0)
+    End Sub
+
+    Private Sub IngresarClienteToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles IngresarClienteToolStripMenuItem.Click
+        Clientes.Show()
+    End Sub
+
+    Private Sub btnPagocliente_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPagocliente.Click
+        Clientes.Show()
+        Clientes.tabcliente.SelectTab(1)
+    End Sub
+End Class
---
+++ ElvirasGYM/ElvirasGYM/Validar.vb
@@ -0,0 +1,79 @@
+﻿Imports System.Text.RegularExpressions
+Public NotInheritable Class Validar
+
+    Public Shared Sub numDecimal(ByRef key As Char)
+        Select Case (Asc(key))
+            Case 8  ' borrar
+            Case 48 To 57     '  numeros
+            Case Else
+                key = Chr(0)
+        End Select
+    End Sub
+
+    Public Shared Sub letrasAndDecimal(ByRef key As Char)
+        Select Case (Asc(key))
+            Case 8  ' borrar
+            Case 13  ' enter
+            Case 48 To 57     '  numeros
+            Case 65 To 90     '  letra Mayusculas
+            Case 67 To 122     '  letra Minusculas
+            Case Else
+                key = Chr(0)
+        End Select
+    End Sub
+
+    Public Shared Sub letras(ByRef key As Char)
+        Select Case (Asc(key))
+            Case 8  ' borrar
+            Case 32
+            Case 65 To 90     '  letra Mayusculas
+            Case 67 To 122     '  letra Minusculas
+            Case Else
+                key = Chr(0)
+        End Select
+    End Sub
+
+
+    Public Shared Sub telefono(ByRef key As Char)
+        Select Case (Asc(key))
+            Case 8  ' borrar
+            Case 48 To 57     ' numeros
+            Case 40 To 41     '  parentesis
+            Case 45 ' guion
+            Case Else
+                key = Chr(0)
+        End Select
+    End Sub
+
+    Public Shared Sub correo(ByRef key As Char)
+        Select Case (Asc(key))
+            Case 8  ' borrar
+            Case 48 To 57 ' numeros
+            Case 65 To 90     '  letra Mayusculas
+            Case 67 To 122     '  letra Minusculas
+            Case 64 ' arroba
+            Case 46 ' punto
+            Case Else
+                key = Chr(0)
+        End Select
+    End Sub
+
+    Public Shared Sub comentario(ByRef key As Char)
+        Select Case (Asc(key))
+            Case 8  ' borrar
+            Case 32
+            Case 48 To 57 ' numeros
+            Case 65 To 90     '  letra Mayusculas
+            Case 67 To 122     '  letra Minusculas
+            Case 64 ' arroba
+            Case 46 ' punto
+            Case Else
+                key = Chr(0)
+        End Select
+    End Sub
+
+    Public Shared Function validaCorreo(ByVal mail As String) As Boolean
+        Return Regex.IsMatch(mail, "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$")
+    End Function
+
+End Class
